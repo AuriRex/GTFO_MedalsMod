26575d46467618e13e9784450fee1edf2ca69be6
AuriRex <xaurirexx@gmail.com>
2025-06-05 23:03:54 +0200

    Ported to new template; removed BepInEx assemblies etc

--- GTFO_MedalsMod/Data/SavedMedals.cs
+++ GTFO_MedalsMod/Data/SavedMedals.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace GTFOMedalsMod.Data;
+namespace MedalsMod.Data;
 
 /// <summary>
 /// This is a class that can store the medals that 
---
+++ GTFO_MedalsMod/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using MedalsMod;
+
+[assembly: AssemblyVersion(Plugin.VERSION)]
+[assembly: AssemblyFileVersion(Plugin.VERSION)]
+[assembly: AssemblyInformationalVersion(Plugin.VERSION)]
+
+namespace MedalsMod;
+
+[BepInPlugin(GUID, MOD_NAME, VERSION)]
+public class Plugin : BasePlugin
+{
+    public const string GUID = "dev.Tgb03.gtfo.GTFO_MedalsMod";
+    public const string MOD_NAME = ManifestInfo.TSName;
+    public const string VERSION = ManifestInfo.TSVersion;
+
+    internal static ManualLogSource L;
+
+    private static readonly Harmony _harmony = new(GUID);
+
+    public override void Load()
+    {
+        L = Log;
+
+        _harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        L.LogInfo("Plugin loaded!");
+    }
+}
