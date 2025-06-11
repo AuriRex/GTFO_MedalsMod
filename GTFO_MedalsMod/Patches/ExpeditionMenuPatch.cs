eedbae373d758973865d4e98aedd7ad7945aabd6
Tgb03 <69427849+Tgb03@users.noreply.github.com>
2025-06-11 16:13:05 +0200

    Help Auri

--- GTFO_MedalsMod/Patches/ExpeditionMenuPatch.cs
+++ GTFO_MedalsMod/Patches/ExpeditionMenuPatch.cs
@@ -6,9 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 using CellMenu;
 using GameData;
+using GameEvent;
+using GTFO.API;
 using HarmonyLib;
+using MedalsMod.Data;
 using MedalsMod.GameObj;
+using Player;
+using TMPro;
+using UnityEngine;
 using static Il2CppSystem.Globalization.CultureInfo;
+using Time = MedalsMod.Data.Time;
 
 namespace MedalsMod.Patches;
 
@@ -22,7 +29,7 @@ internal class ExpeditionMenuPatch
         typeof(ExpeditionInTierData)
         ])]
     [HarmonyPostfix]
-    public static void Postfix(pActiveExpedition activeExpedition, ExpeditionInTierData expeditionInTierData) 
+    public static void PostfixLobby(pActiveExpedition activeExpedition, ExpeditionInTierData expeditionInTierData) 
     {
         var exp_index = activeExpedition.expeditionIndex + 1;
         var descriptive = expeditionInTierData.Descriptive;
@@ -30,6 +37,41 @@ internal class ExpeditionMenuPatch
             (!descriptive.SkipExpNumberInName ? exp_index.ToString() : "");
 
         LevelSelectText.UpdateMedalText(levelName);
+    }
+}
+
+[HarmonyPatch(typeof(CM_PageExpeditionSuccess), "OnEnable")]
+internal static class Inject_CM_Expedition
+{
+    private static GameObject icon;
+
+    private static void Postfix(CM_PageExpeditionSuccess __instance)
+    {
+        Time? time = TimeCollector.GetLastTime();
+        string? exp = TimeCollector.GetLastLevel();
+
+        var medal = MedalRegistry.AllMedals[exp].GetMedal(time);
+
+        if (medal == null) { return; }
+
+        try {
+
+            // add medal here somehow????
+            // code info: 
+            //
+            // `MedalImages.GetSpriteFromMedal((Medal)medal)` returns a sprite with the correct medal
+            // `TimeCollector.GetLastLevel()` grabs the last level name. I do not know how to grab
+            //          the objectives existent in the level sadly.
+            // 
+            // 
+
+            // this adds the text for the medal in the color of the medal. I will probably delete this as it is clutter
+            __instance.m_expeditionName.text += $"   <{MedalColors.GetMedalColor(medal)}>{medal}</color>";
+            __instance.m_expeditionName.ForceMeshUpdate();
+            
+        } catch (Exception e) {
+            Plugin.L.LogError(e);
+        }
     }
 }
 
